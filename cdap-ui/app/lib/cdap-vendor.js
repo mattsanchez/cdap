c537b0964d0395c9c07b7eac1bf4fdb2619174c2
Ajai <ajainarayanan@gmail.com>
2017-01-03 19:25:57 -0800

    Adds vendor library entry points for cdap, wrangler and common webapps

---
+++ cdap-ui/app/lib/cdap-vendor.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+require('whatwg-fetch');
+require('react');
+require('react-dom');
+require('redux');
+require('lodash');
+require('classnames');
+require('node-uuid');
+require('sockjs-client');
+require('rx');
+require('reactstrap');
+require('react-addons-css-transition-group');
+require('i18n-react');
+require('fuse.js');
+require('react-dropzone');
+require('react-redux');
+require('react-router');
+require('moment');
+require('react-file-download');
+require('mousetrap');
+require('papaparse');
+require('rx-dom');
+require('d3');
+require('chart.js');
+require('cdap-avsc');
